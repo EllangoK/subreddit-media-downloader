5d1d563d690188b001fb29f7d0f6cd3b4ebdccbd
EllangoK <ellangok@bxscience.edu>
2020-06-30 11:35:06 -0400

    Progress

--- subreddit_scrape.py
+++ subreddit_scrape.py
@@ -1,9 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 from psaw import PushshiftAPI
 from bs4 import BeautifulSoup
 import requests
+import shutil
+import math
 import sys
 import os
 
@@ -20,25 +22,22 @@ Format:
 Example:
     $ python subreddit_scrape.py pics cat
 
+Files are saved to a folder with the same name as the search term.
 If you omit the upvote threshold, a praw.ini file will not be required
 Thus, images and videos can be gathered without authentication
 
 Use quotes if more than one word
 """
 
-def progressBar(iterable, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r"):
-    total = len(iterable)
-    def printProgressBar(iteration):
-        percent = ("{0:." + str(decimals) + "f}").format(100 *
-                                                         (iteration / float(total)))
-        filledLength = int(length * iteration // total)
-        bar = fill * filledLength + '-' * (length - filledLength)
-        print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
-    printProgressBar(0)
-    for i, item in enumerate(iterable):
-        yield item
-        printProgressBar(i + 1)
-    print()
+def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r"):
+    percent = ("{0:." + str(decimals) + "f}").format(100 *
+                                                     (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=printEnd)
+
+    if iteration == total:
+        print()
 
 def threshold(data, upvote_thresh):
     return [item for item in data if int(item[3]) > upvote_thresh]
@@ -63,8 +62,15 @@ def source_url(link):
 
 def download_images(folder_name, file_names_and_download_links):
     folder_name = os.path.join(os.getcwd(), folder_name)
+    if os.path.isdir(folder_name):
+        shutil.rmtree(folder_name)
     os.mkdir(folder_name)
-    for item in progressBar(file_names_and_download_links, prefix='Progress:', suffix='Complete', length=60):
+
+    printProgressBar(0, len(file_names_and_download_links), prefix='Progress:',
+                     suffix='Complete', length=60)
+    for i, item in enumerate(file_names_and_download_links):
+        printProgressBar(i + 1, len(file_names_and_download_links), prefix='Progress:',
+                         suffix='Complete', length=60)
         try:
             urllib.request.urlretrieve(item[1], folder_name + '\\' + item[0])
         except:
@@ -77,7 +83,7 @@ def search_pushshift(subreddit_name, search_term):
                                             filter=['id', 'author',
                                                     'title', 'url'],
                                             limit=5000))
-
+    
     return [item for item in psaw_search if 'reddit.com/r/' not in item[4]]
 
 def pushshift_based(results):
@@ -92,9 +98,13 @@ def praw_based(results):
         fullnames.append('t3_' + id)
 
     useful_info = []
-    for submission in r.info(fullnames):
+    printProgressBar(0, len(fullnames), prefix='Progress:',
+                     suffix='Complete', length=60)
+    for i, submission in enumerate(r.info(fullnames)):
         info = [submission.id, submission.title,
                 submission.url, submission.score]
+        printProgressBar(i + 1, len(fullnames), prefix='Progress:',
+                         suffix='Complete', length=60)
         if None not in info:
             useful_info.append(info)
         else:
@@ -114,18 +124,28 @@ if __name__ == '__main__':
         except:
             print("Error: Upvote threshold is not an integer")
             sys.exit()
-    
+
+    print("Searching for" + ' "' + args[1] +
+          '" ' + "on r/" + args[0] + " for images and videos", end='')
+    if upvote_thresh:
+        print(" with an upvote threshold of " + args[2])
+    else:
+        print()
     pushshift_results = search_pushshift(args[0], args[1])
+
     if len(pushshift_results) == 0:
         print("No results found. Check to make sure your properly spelled the subreddit name and search term")
 
     if not upvote_thresh:
         information = pushshift_based(pushshift_results)
         file_names_and_download_links = [(str(item[0]) + '.' + source_url(item[2]).split('.')[-1], source_url(item[2])) for item in information if source_url(item[2])]
+        print("Downloading " + str(len(file_names_and_download_links)) + " Images")
         download_images(args[1], file_names_and_download_links)
     else:
+        print("Gathering Upvote Data")
         information = threshold(praw_based(pushshift_results), upvote_thresh)
         file_names_and_download_links = [(str(item[3]) + ',' + str(item[0]) + '.' + source_url(item[2]).split(
             '.')[-1], source_url(item[2])) for item in information if source_url(item[2])]
+        print("Downloading " + str(len(file_names_and_download_links)) + " Images out of a possible " + str(len(pushshift_results)) + " Images")
         download_images(args[1], file_names_and_download_links)
 
